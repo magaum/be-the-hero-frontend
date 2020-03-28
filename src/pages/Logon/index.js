a4ac4ae38bacd5dc328673e8dc4341cb6eebf116
Weslei Luiz de Paula Pinto <weslei.luiz@softwillians.com.br>
2020-03-28 20:28:15 -0300

    add pagina profile

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css'
+import herosImg from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function Logon() {
     return (
-        <h1>Hello world</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be the Hero logo" />
+
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua Id" />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <Link className="back-link" to="register">
+                        <FiLogIn size={18} color="#e02041" />
+                        Não tenho cadastro
+                    </Link>
+
+                </form>
+            </section>
+
+            <img src={herosImg} alt="Heroes" />
+        </div>
     );
 }
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
+import './styles.css'
+import logoImg from '../../assets/logo.svg'
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be the Hero logo" />
+                <span>Bem vinda, APAD</span>
+
+                <Link className="button" to="/casos/novo">Cadastrar novo caso</Link>
+
+                <button type="button">
+                    <FiPower size={18} color="#e02041" />
+                </button>
+            </header>
+
+            <h1>Casos cadastrados</h1>
+
+            <ul>
+                <li>
+                    <strong>CARAI</strong>
+                    <p>Casos de familia</p>
+
+                    <strong>Descrição</strong>
+                    <p>Descrição mais descrita</p>
+
+                    <strong>Valor: </strong>
+                    <p>R$ 8984984984894984</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li><li>
+                    <strong>CARAI</strong>
+                    <p>Casos de familia</p>
+
+                    <strong>Descrição</strong>
+                    <p>Descrição mais descrita</p>
+
+                    <strong>Valor: </strong>
+                    <p>R$ 8984984984894984</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li><li>
+                    <strong>CARAI</strong>
+                    <p>Casos de familia</p>
+
+                    <strong>Descrição</strong>
+                    <p>Descrição mais descrita</p>
+
+                    <strong>Valor: </strong>
+                    <p>R$ 8984984984894984</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li><li>
+                    <strong>CARAI</strong>
+                    <p>Casos de familia</p>
+
+                    <strong>Descrição</strong>
+                    <p>Descrição mais descrita</p>
+
+                    <strong>Valor: </strong>
+                    <p>R$ 8984984984894984</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
